d72c2814a254f48c7566576bd4e5f47398835128
Derrick Lee <128185320+DerrickL25@users.noreply.github.com>
2023-03-18 13:35:08 -0700

    Update randstate.c

--- randstate.c
+++ randstate.c
@@ -7,10 +7,12 @@
 #include "randstate.h"
 
 gmp_randstate_t state;
-void randstate_init(uint64_t seed) {
+
+void randstate_init(uint64_t seed) {                       // Initializes the random state needed for RSA key generation operations; can be seeded
   gmp_randinit_mt(state);
   gmp_randseed_ui(state, seed);
   srandom(seed);
 }
 
-void randstate_clear(void) { gmp_randclear(state); }
+void randstate_clear(void) { gmp_randclear(state); }       // Frees any memory used by the initialized random state
+
